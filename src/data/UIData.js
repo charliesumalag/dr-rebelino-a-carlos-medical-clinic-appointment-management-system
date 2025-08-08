ce2e55681e8b32f22f213868d1aa90d602d721c5
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-08-08 15:50:46 +0800

    organizing ui's data.

---
+++ src/data/UIData.js
@@ -0,0 +1,24 @@
+import { MdDifference, MdWheelchairPickup, MdCheckCircleOutline, MdSchedule } from 'react-icons/md';
+
+export const upcomingSectionData = [
+  {
+    headerText: 'Upcoming Appointments until Next Friday',
+    texts: ['Heres quick access to upcoming appointments until 7 days','More details available in appointments section.'],
+    lists: ['Appointment number', 'Patient name', 'Doctor', 'Session'],
+    buttonLable: 'Show all appointments'
+  },
+  {
+    headerText: 'Upcoming Sessions until Next Friday',
+    texts: ['Heres quick access of upcoming sessions that scheduled until 7 days', 'Add, remove and many features available in schedule section'],
+    lists: ['Session title', 'Doctor', 'Scedule Date and Time'],
+    buttonLable: 'Show all sessiona'
+  }
+];
+
+
+export const boxListData = [
+  {text: 'Doctors', icon: MdDifference },
+  { text: 'Patients', icon: MdWheelchairPickup },
+  { text: 'New Booking', icon: MdCheckCircleOutline },
+  { text: "Today's Session", icon: MdSchedule },
+];
--- src/pages/administrator/dashboard/dashboardHome/Dashboard.jsx
+++ src/pages/administrator/dashboard/dashboardHome/Dashboard.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { MdCalendarMonth } from "react-icons/md";
-import { MdDifference } from "react-icons/md";
-import { MdWheelchairPickup } from "react-icons/md";
-import { MdCheckCircleOutline } from "react-icons/md";
-import { MdSchedule } from "react-icons/md";
+import { MdCalendarMonth, MdSearch } from "react-icons/md";
 import styles from "./Dashboard.module.css";
-import { MdSearch } from "react-icons/md";
 import Button from "../../../../components/dashboard/button/Button";
 import HeaderText from "../../../../components/dashboard/headerText/HeaderText";
 import Boxes from "../../../../components/dashboard/boxes/Boxes";
 import Upcomming from "../../../../components/dashboard/upcoming/Upcomming";
+import {upcomingSectionData, boxListData}  from "../../../../data/UIData";
 
 const Dashboard = () => {
   return (
@@ -32,28 +28,20 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-
       <div>
         <HeaderText text='Status' variant='headerTextBlack'  />
       </div>
       <ul className={styles.lists}>
-        <li className={styles.list}>
-          <Boxes text='Doctors' icon={MdDifference}/>
-        </li>
-        <li className={styles.list}>
-          <Boxes text='Patients' icon={MdWheelchairPickup}/>
-        </li>
-        <li className={styles.list}>
-          <Boxes text='New Booking' icon={MdCheckCircleOutline}/>
-        </li>
-        <li className={styles.list}>
-          <Boxes text="Today's Session" icon={MdSchedule}/>
-        </li>
+        {boxListData.map(({ text, icon }, index) => (
+          <li key={index} className={styles.list}>
+            <Boxes text={text} icon={icon} />
+          </li>
+        ))}
       </ul>
-      {/* Upcoming */}
       <div className={styles.upcomingList}>
-        <Upcomming headerText='Upcoming Appointments until Next Friday' texts={['Heres quick access to upcoming appointments until 7 days', 'More details available in appointments section.']} lists={['Appointment number', 'Patient name', 'Doctor', 'Session']} buttonLable='Show all appointments' />
-        <Upcomming headerText='Upcoming Sessions until Next Friday' texts={['Heres quick access of upcoming sessions that scheduled until 7 days', 'Add, remove and many features available in schedule section']} lists={['Session title', 'Doctor', 'Scedule Date and Time']} buttonLable='Show all sessiona' />
+        {upcomingSectionData.map((data, idx) => (
+          <Upcomming key={idx} {...data} />
+        ))}
       </div>
     </div>
 
